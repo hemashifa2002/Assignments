f626d3982f28d6091a53336ac6606ff20414859c
HEMA P <99114157+hemashifa2002@users.noreply.github.com>
2025-10-09 20:04:38 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,22 @@
+package org.testleaf;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear =2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("Version:" + version);
+		System.out.println("Developer:" + developer);
+		System.out.println("Is Beta:" + isBeta);
+		System.out.println("Release Year:" + releaseYear);
+		System.out.println("ShortcutKey:" + shortcutKey);	
+	
+		
+	}
+
+}
